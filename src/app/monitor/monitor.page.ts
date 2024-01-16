4ce43d850fb0f9b0be83d7795a6395f4971c59b9
paperstreets <naviholod@gmail.com>
2024-01-16 09:38:08 +0200

    final commit

--- src/app/monitor/monitor.page.ts
+++ src/app/monitor/monitor.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.page.html',
   styleUrls: ['./monitor.page.scss'],
 })
-export class MonitorPage implements OnInit {
+export class MonitorPage implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.loadScript('https://public.tableau.com/javascripts/api/viz_v1.js');
   }
 
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
--- src/app/reports/reports.page.ts
+++ src/app/reports/reports.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.page.html',
   styleUrls: ['./reports.page.scss'],
 })
-export class ReportsPage implements OnInit {
+export class ReportsPage implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.loadScript('https://public.tableau.com/javascripts/api/viz_v1.js');
   }
 
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
